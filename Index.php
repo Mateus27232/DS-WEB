9c3a41603e24becf939787086fa0d52e585f455d
Mateus27232 <mcovre5@gmail.com>
2026-02-03 14:22:25 -0300

    1 commit 2026

---
+++ Index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Página Principal</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Mateus Covre</h1>
+        <h2>Página principal do seu servidor</h2>
+
+        <div class="menu">
+            <a href="/POO/">Aulas POO</a>
+            <a href="#">2</a>
+        </div>
+    </div>
+</body>
+</html>
---
+++ POO/Index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Aulas POO</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Aulas POO</h1>
+        <h2>Conteúdo de Programação Orientada a Objetos</h2>
+
+        <div class="menu">
+            <a href="#">1</a>
+            <a href="#">2</a>
+            <a href="../" class="voltar">⬅ Voltar</a>
+        </div>
+    </div>
+</body>
+</html>
