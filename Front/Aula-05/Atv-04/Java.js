862851e9fb10efcd86a956803aa1efb3e1788434
Mateus27232 <mcovre5@gmail.com>
2026-02-27 11:04:13 -0300

    sexto commit

--- Front/Aula-05/Atv-04/Java.js
+++ Front/Aula-05/Atv-04/Java.js
@@ -17,6 +17,9 @@ function mudar() {
             '<img src="breno-bidon-drible-corinthians.gif" alt="gif">';
         document.getElementById("gif6").innerHTML =
             '<img src="corinthians.gif" alt="gif">';
+        document.getElementById("gif7").innerHTML =
+            '<img src="fiel-fiel-torcida.gif" alt="gif">';
+        
       
         audio.play();  // toca o áudio
     } else {
@@ -26,8 +29,85 @@ function mudar() {
         document.getElementById("gif4").innerHTML = "";
         document.getElementById("gif5").innerHTML = "";
         document.getElementById("gif6").innerHTML = "";
+        document.getElementById("gif7").innerHTML = "";
+       
 
         audio.currentTime = 0; // volta pro começo do áudio
         audio.pause(); // pausa o áudio
     }
 }
+
+const imagem = document.querySelector(".imagem");
+
+    let mouseX = 0;
+    let mouseY = 0;
+    let rotacao = 0;
+
+    document.addEventListener("mousemove", function(event){
+        mouseX = event.clientX - 30;
+        mouseY = event.clientY - 30;
+
+        // cria rastro
+        let clone = imagem.cloneNode(true);
+        clone.classList.remove("imagem");
+        clone.classList.add("rastro");
+        clone.style.left = mouseX + "px";
+        clone.style.top = mouseY + "px";
+        clone.style.transform = "rotate(" + rotacao + "deg)";
+        document.body.appendChild(clone);
+
+        setTimeout(() => clone.remove(), 500);
+    });
+
+    function animar(){
+        rotacao += 5; // velocidade da rotação
+
+        imagem.style.left = mouseX + "px";
+        imagem.style.top = mouseY + "px";
+        imagem.style.transform = "rotate(" + rotacao + "deg)";
+
+        requestAnimationFrame(animar);
+    }
+
+    animar();
+
+    // cria cópia ao clicar
+    document.addEventListener("click", function(event){
+
+        let clone = imagem.cloneNode(true);
+
+        clone.classList.remove("imagem");
+        clone.classList.add("fixo");
+
+        clone.style.left = (event.clientX - 30) + "px";
+        clone.style.top = (event.clientY - 30) + "px";
+
+        document.body.appendChild(clone);
+    });
+
+function animarTrofeu(){
+
+    x += velocidadeX;
+    y += velocidadeY;
+
+    let larguraTela = window.innerWidth;
+    let alturaTela = window.innerHeight;
+
+    let larguraImg = trofeu.offsetWidth;
+    let alturaImg = trofeu.offsetHeight;
+
+    if(x + larguraImg >= larguraTela || x <= 0){
+        velocidadeX *= -1;
+    }
+
+    if(y + alturaImg >= alturaTela || y <= 0){
+        velocidadeY *= -1;
+    }
+
+    trofeu.style.left = x + "px";
+    trofeu.style.top = y + "px";
+
+    requestAnimationFrame(animarTrofeu);
+}
+
+animarTrofeu();
